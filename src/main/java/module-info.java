67f256b3321d28cdfa87250180ee003ae9949297
termansnake <terman.snake@gmail.com>
2021-10-04 19:44:40 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.exampleGropuid.MainartifactID {
+    requires org.slf4j;             //slf4j-api-2.0.0-alpha1.jar
+    requires org.slf4j.simple;      //slf4j-simple-2.0.0-alpha1.jar & simplelogger.properties
+    requires com.pi4j;
+    requires com.pi4j.plugin.raspberrypi;
+    requires com.pi4j.plugin.pigpio;
+    requires com.pi4j.library.pigpio;
+
+    uses com.pi4j.extension.Extension;
+    uses com.pi4j.provider.Provider;
+
+    // allow access to classes in the following namespaces for Pi4J annotation processing
+    opens com.exampleGropuid to com.pi4j;
+
+    //exports com.exampleGropuid;
+}
